b0c04a4a3bba0938bb5706ac6c6d26dadc88d062
bastien.grisvard <bastien.grisvard@raccourci.fr>
2026-03-27 17:19:20 +0100

    feat(Spring Project): initialize simple spring project

---
+++ emergency-bed-service/src/main/java/fr/medhead/emergency_bed_service/EmergencyBedServiceApplication.java
@@ -0,0 +1,14 @@
+package fr.medhead.emergency_bed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyBedServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyBedServiceApplication.class, args);
+    }
+}
+
---
+++ emergency-bed-service/src/test/java/fr/medhead/emergency_bed_service/EmergencyBedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.medhead.emergency_bed_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyBedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
