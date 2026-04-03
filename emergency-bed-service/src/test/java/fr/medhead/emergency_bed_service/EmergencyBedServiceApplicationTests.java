0533d54031dee6d3a314c25e558af6ed88b3e22e
bastien.grisvard <bastien.grisvard@raccourci.fr>
2026-04-03 09:52:17 +0200

    test(Integration Test): fix context load test

--- emergency-bed-service/src/test/java/fr/medhead/emergency_bed_service/EmergencyBedServiceApplicationTests.java
+++ emergency-bed-service/src/test/java/fr/medhead/emergency_bed_service/EmergencyBedServiceApplicationTests.java
@@ -2,12 +2,20 @@ package fr.medhead.emergency_bed_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class EmergencyBedServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
+    @Test
+    void contextLoads() {
+    }
 }
