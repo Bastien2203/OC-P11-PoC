66f66a9267d1072f6c2e34a53457c5b81c52311b
bastien.grisvard <bastien.grisvard@raccourci.fr>
2026-05-14 12:32:27 +0200

    tests(e2e): add e2e tests

--- front-end/e2e/example.spec.ts
+++ front-end/e2e/example.spec.ts
@@ -4,9 +4,39 @@ import { test, expect } from '@playwright/test';
 test('User can view speciality groups fetched from backend', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.locator('h1')).toHaveText('Medhead');
-
   await expect(page.getByText('Chargement des spécialités...')).toBeHidden();
 
-  await expect(page.getByText('Endodontie')).toBeVisible();
+  await expect(page.getByText('Anesthésie')).toBeVisible();
+});
+
+
+test('User can choose a speciality, enter its address, and book a bed in an available hospital', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('a').filter({ hasText: 'Groupe de médecine générale' }).click();
+  await page.locator('a').filter({ hasText: 'Cardiologie' }).click();
+  await page.getByRole('button', { name: 'Suivant' }).click();
+  await page.getByRole('textbox', { name: 'Commencez à taper une adresse' }).click();
+  await page.getByRole('textbox', { name: 'Commencez à taper une adresse' }).fill('17 Rue paul émile victor la rochelle');
+  await page.getByText('Rue Paul-Émile Victor 17000 La Rochelle17, Charente-Maritime, Nouvelle-Aquitaine').click();
+  await page.getByRole('button', { name: 'Rechercher' }).click();
+  await expect(page.getByRole('main')).toContainText('2');
+  await expect(page.locator('h2')).toContainText('Hôpital trouvé !');
+  await page.getByRole('button', { name: 'Réserver un lit' }).click();
+  await expect(page.locator('h2')).toContainText('Réservation confirmée');
+  await expect(page.getByRole('strong')).toContainText('Hôpital Fred Brooks');
+  await expect(page.getByRole('button')).toContainText('Retour à l\'accueil');
+});
+
+
+test('User choose an anavailable speciality, so a message indicate that no hospital found with this speciality', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('a').filter({ hasText: 'Oncologie clinique' }).click();
+  await page.locator('a').click();
+  await page.getByRole('button', { name: 'Suivant' }).click();
+  await page.getByRole('textbox', { name: 'Commencez à taper une adresse' }).click();
+  await page.getByRole('textbox', { name: 'Commencez à taper une adresse' }).fill('17 Rue paul émile victor la rochelle');
+  await page.getByText('Rue Paul-Émile Victor 17000 La Rochelle17, Charente-Maritime, Nouvelle-Aquitaine').click();
+  await page.getByRole('button', { name: 'Rechercher' }).click();
+  await expect(page.locator('h2')).toContainText('Aucun établissement');
+  await expect(page.getByRole('button')).toContainText('Nouvelle recherche');
 });
